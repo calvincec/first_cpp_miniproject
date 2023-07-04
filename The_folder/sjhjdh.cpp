2994d2357eab59388d29c2fec6bdf0c756b12b95
calvince <calyndemo16@gmail.com>
2023-07-04 08:54:36 +0300

    The folder

---
+++ The_folder/sjhjdh.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+int main(){
+ int arr[] = {25,4,4,43,4,4,4};
+ cout<<sizeof(arr)/sizeof(arr[0]);
+}
